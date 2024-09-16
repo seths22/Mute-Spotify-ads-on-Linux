c71afa989cf20d9363545ad48c3b76816a1bc2af
Seth Shively <seth2000@protonmail.com>
2024-09-15 20:06:49 -0500

    Now unmutes when Spotify is not running

--- mute_spotify_ads.sh
+++ mute_spotify_ads.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
 prevName=""
+spotifyRunning=0
 while true; do
-	# get Spotify playing song name
-	
-	song=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2`
+    numSpotifyProc=`ps aux | grep -c /usr/share/spotify`
+    if [ "$numSpotifyProc" != "1" ]; then
+        spotifyRunning=1
+	    # get Spotify playing song name
+	    
+	    song=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2`
 
-    artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2`
-	
-	if [[ "$song" != "$prevName" ]]; then
-		songChanged=1
-	else
-		songChanged=0
-	fi
+        artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2`
+	    
+	    if [[ "$song" != "$prevName" ]]; then
+		    songChanged=1
+	    else
+		    songChanged=0
+	    fi
 
-	if [ $songChanged == 1 ]; then
-		echo "${artist}: ${song}"
-	fi
+	    if [ $songChanged == 1 ]; then
+		    echo "${artist}: ${song}"
+	    fi
 
-	if [[ "$song" = *"Advertisement"* || "$artist" = *"Advertisement"* || "$song" = *"Spotify"* || "$artist" = *"Spotify"* || "$song" = "" || "$artist" = "" ]]; then
-		pactl set-sink-mute @DEFAULT_SINK@ 1
-		if [ $songChanged == 1 ]; then
-			echo "Muting"
-		fi
-	else
-		pactl set-sink-mute @DEFAULT_SINK@ 0
-		if [ $songChanged == 1 ]; then
-			echo "Unmuting"
-		fi
-	fi
-	prevName="$song"
-	sleep 1
+	    if [[ "$song" = *"Advertisement"* || "$artist" = *"Advertisement"* || "$song" = *"Spotify"* || "$artist" = *"Spotify"* || "$song" = "" || "$artist" = "" ]]; then		    
+		    if [ $songChanged == 1 ]; then
+			    echo "Muting"
+                pactl set-sink-mute @DEFAULT_SINK@ 1
+		    fi
+	    else		   
+		    if [ $songChanged == 1 ]; then
+			    echo "Unmuting"
+                pactl set-sink-mute @DEFAULT_SINK@ 0
+		    fi
+	    fi
+	    prevName="$song"
+    else
+        if [ $spotifyRunning == 1 ]; then
+            echo "Spotify is not running : unmuting"
+            spotifyRunning=0
+            pactl set-sink-mute @DEFAULT_SINK@ 0
+        fi
+    fi
+    sleep 0.5
 done
