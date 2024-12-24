54384d864b25e33f862371ecbe9efe6596e05783
Seth Shively <seth2000@protonmail.com>
2024-12-24 11:25:07 -0600

    Now does not produce terminal output (this can be undone by uncommenting lines in mute_spotify_ads.sh)

--- mute_spotify_ads.sh
+++ mute_spotify_ads.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
-
 prevName=""
 spotifyStarted=0
 muted=0
 while true; do
-    numSpotifyProc=`ps aux | grep -c /usr/share/spotify`
-    if [ "$numSpotifyProc" != "1" ]; then
+    numSpotifyProc=`pgrep -fc /usr/share/spotify`
+    if [ $numSpotifyProc -gt 0 ]; then
         spotifyStarted=1
-	    # get Spotify playing song name
-	    
+	    # get Spotify playing song name	    
 	    song=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2`
 
         artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2`
-	    
-	    if [[ "$song" != "$prevName" ]]; then
-		    songChanged=1
-	    else
-		    songChanged=0
-	    fi
 
-	    if [[ "$artist" = "" ]]; then	    
+	    if [ "$artist" = "" ]; then	    
             if [ $muted == 0 ]; then
-                echo "Advertisement: Muting"
                 pactl set-sink-mute @DEFAULT_SINK@ 1
                 muted=1
             fi
 	    else		   
-		    if [ $songChanged == 1 ]; then
-			    echo "${artist}: ${song}"                
-		    fi
             if [ $muted == 1 ]; then
-                echo "Unmuting"
                 pactl set-sink-mute @DEFAULT_SINK@ 0
                 muted=0
             fi
@@ -45,5 +32,5 @@ while true; do
             exit 0
         fi
     fi
-    sleep 0.5
+    sleep 0.2
 done
